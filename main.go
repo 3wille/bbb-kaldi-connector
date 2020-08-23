57a2199414b42d3238b782ad7a072b92612d567d
Frederik Wille <frederik@wille.hamburg>
2020-08-23 23:00:08 +0200

    full call cycle from invite to hangup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net"
 	"net/url"
@@ -19,7 +20,8 @@ import (
 func sipCall() {
 	host := "ltbbb1.informatik.uni-hamburg.de"
 	room := "echo72278"
-	sipURL := url.URL{Scheme: "wss", Host: host, Path: "/ws", RawQuery: "sessionToken=wf7r73gzi4hyascr"}
+	sessionToken := "wed7bgjka4kslkn4"
+	sipURL := url.URL{Scheme: "wss", Host: host, Path: "/ws", RawQuery: fmt.Sprintf("sessionToken=%v", sessionToken)}
 	log.Print(sipURL.String())
 	sipConnection, _, err := websocket.DefaultDialer.Dial(sipURL.String(), nil)
 	if err != nil {
@@ -59,6 +61,76 @@ func sipCall() {
 
 	sendSipInvite(invite, sipConnection)
 
+	waitFor100Trying(sipConnection)
+
+	// Receive 200 OK.
+	sipConnection.SetReadDeadline(time.Now().Add(5 * time.Second))
+	_, message, err := sipConnection.ReadMessage()
+	if err != nil {
+		log.Fatal("read 200 ok:", err)
+	}
+	log.Printf("<<< \n%s\n", string(message))
+	msg, err := sip.ParseMsg(message)
+	if err != nil {
+		log.Fatal("parse 200 ok:", err)
+	}
+	if !msg.IsResponse() || msg.Status != 200 || msg.Phrase != "OK" {
+		log.Fatal("wanted 200 ok but got:", msg.Status, msg.Phrase)
+	}
+
+	// Figure out where they want us to send RTP.
+	var rrtpaddr *net.UDPAddr
+	if ms, ok := msg.Payload.(*sdp.SDP); ok {
+		rrtpaddr = &net.UDPAddr{IP: net.ParseIP(ms.Addr), Port: int(ms.Audio.Port)}
+	} else {
+		log.Fatal("200 ok didn't have sdp payload")
+	}
+	log.Print(rrtpaddr)
+
+	// Acknowledge the 200 OK to answer the call.
+	var ack sip.Msg
+	ack.Request = invite.Request
+	ack.From = msg.From
+	ack.To = msg.To
+	ack.CallID = msg.CallID
+	ack.Method = "ACK"
+	ack.CSeq = msg.CSeq
+	ack.CSeqMethod = "ACK"
+	ack.Via = msg.Via
+	var b bytes.Buffer
+	ack.Append(&b)
+	err = sipConnection.WriteMessage(websocket.TextMessage, b.Bytes())
+	if err != nil {
+		log.Fatal("send ack over websocket: ", err)
+	}
+
+	var bye sip.Msg
+	bye.Method = "BYE"
+	bye.CSeqMethod = "BYE"
+	bye.CSeq++
+	b.Reset()
+	bye.Append(&b)
+	err = sipConnection.WriteMessage(websocket.TextMessage, b.Bytes())
+	if err != nil {
+		log.Fatal("send ack over websocket: ", err)
+	}
+
+	// Wait for acknowledgment of hangup.
+	sipConnection.SetReadDeadline(time.Now().Add(time.Second * 2))
+	_, message, err = sipConnection.ReadMessage()
+	if err != nil {
+		log.Fatal(err)
+	}
+	msg, err = sip.ParseMsg(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !msg.IsResponse() || msg.Status != 200 || msg.Phrase != "OK" {
+		log.Fatal("wanted bye response 200 ok but got:", msg.Status, msg.Phrase)
+	}
+}
+
+func waitFor100Trying(sipConnection *websocket.Conn) {
 	// Receive provisional 100 Trying.
 	sipConnection.SetReadDeadline(time.Now().Add(time.Second * 2))
 	// memory := make([]byte, 2048)
@@ -81,27 +153,12 @@ func sendSipInvite(invite *sip.Msg, sipConnection *websocket.Conn) {
 	// Turn invite message into a packet and send via UDP socket.
 	var b bytes.Buffer
 	invite.Append(&b)
-	// if amt, err := sipConnection.Write(b.Bytes()); err != nil || amt != b.Len() {
-	// 	t.Fatal(err)
-	// }
 	err := sipConnection.WriteMessage(websocket.TextMessage, b.Bytes())
-	// err = writeMessage(websocket.TextMessage, b.Bytes(), sipConnection)
 	if err != nil {
 		log.Fatal("send sip over websocket: ", err)
 	}
-	// log.Print("send sip over websocket done")
-	log.Printf(">>>\n%s\n", b.String())
-}
-
-func writeMessage(messageType int, data []byte, c *websocket.Conn) error {
-	w, err := c.NextWriter(messageType)
-	if err != nil {
-		return err
-	}
-	if _, err = w.Write(data); err != nil {
-		return err
-	}
-	return w.Close()
+	log.Print("send sip over websocket done")
+	// log.Printf(">>>\n%s\n", b.String())
 }
 
 func buildSipInvite(room string, host string, rtpaddr *net.UDPAddr) *sip.Msg {
@@ -120,7 +177,7 @@ func buildSipInvite(room string, host string, rtpaddr *net.UDPAddr) *sip.Msg {
 			Protocol:  "SIP",
 			Version:   "2.0",
 			Transport: "WSS",
-			Host:      "mjcbf3j4j6bs.invalid",
+			Host:      "mjddf3j4c6bs.invalid",
 			// Port:  uint16(laddr.Port),
 			Param: &sip.Param{Name: "branch", Value: util.GenerateBranch()},
 		},
@@ -128,7 +185,7 @@ func buildSipInvite(room string, host string, rtpaddr *net.UDPAddr) *sip.Msg {
 			Display: "SIP Test",
 			Uri: &sip.URI{
 				Scheme: "sip",
-				User:   "w_bsauub1dfst0_7-bbbID-Frederik%20Wille",
+				User:   "w_bsaucdftt0_7-bbbID-Frederik%20Wille",
 				Host:   host, // laddr.IP.String(),
 				// Port:   uint16(laddr.Port),
 			},
@@ -146,7 +203,7 @@ func buildSipInvite(room string, host string, rtpaddr *net.UDPAddr) *sip.Msg {
 			Uri: &sip.URI{
 				Scheme: "sip",
 				User:   "3hq5f4mr",
-				Host:   "mjcbf3j4j6bs.invalid",
+				Host:   "mjdde3j4j6bs.invalid",
 				// Host:   laddr.IP.String(),
 				// Port:   uint16(laddr.Port),
 			},
